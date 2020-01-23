92b33f9f12a1186d9f6d2ab2abedb334aaf1eac8
Justin Chester <jchester@git.socs.uoguelph.ca>
2020-01-22 22:56:56 -0500

    Change landing page

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,27 +3,32 @@ import React, { Component } from 'react';
 export default class header extends Component {
   render() {
     return (
-      <header>
-       
-       <div className="logo">
-         LOGO
-       </div>
-
-       <nav>
-         <ul>
-           <li>
-             <a href="#">Home</a>
-             </li>
-           <li>
-             <a href="#">Products</a>
-             </li>
-           <li>
-             <a href="#">Contact</a>
-           </li>
-         </ul>
-       </nav>
-
-      </header>
+      <div>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top d-flex">
+        <div class="container">
+          <a class="navbar-brand" href="#">Co-op Reports</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#" routerLink="/">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" routerLink="/aboutme">About Me</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" routerLink="/workterms">Work Terms</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
     )
   }
 }
--- src/components/pages/pages.js
+++ src/components/pages/pages.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react';
 export default class pages extends Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div>
         Home page content here
-        hi
       </div>
     )
   }
